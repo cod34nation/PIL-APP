81ed74b4106c5404ece8e9f992fc2ba8d8e46722
cod34nation <afrizalwibisono87@gmail.com>
2016-11-29 14:18:03 +0700

    29 November 2016

---
+++ pantauInfoLab2/app/src/main/java/com/example/afrizal/pantauinfolab/app/AppConfig.java
@@ -0,0 +1,15 @@
+package com.example.afrizal.pantauinfolab.app;
+
+/**
+ * Created by AFRIZAL on 11/27/2016.
+ */
+/**
+ * Created by Windows10 on 04/04/2016.
+ */
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://192.168.43.35/aplikasi-client-server/config_client/API/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://192.168.43.35/aplikasi-client-server/config_client/API/register.php";
+}
